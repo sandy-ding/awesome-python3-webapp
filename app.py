2650a5899e60a627f8a18b089c6957cc1da125c0
Sandy <dhl15821939286@gmail.com>
2018-11-25 21:35:52 +0800

    Day11

--- app.py
+++ app.py
@@ -79,9 +79,9 @@ async def auth_factory(app, handler):
             if user:
                 logging.info('set current user: %s' % user.email)
                 request.__user__ = user
-            if request.path.startswith('/manage/') and (request.__user__ is not None or not request.__user__.admin):
-                return web.HTTPFound('/signin')
-            return(await handler(request))
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return(await handler(request))
     return auth
 
 async def data_factory(app, handler):
@@ -172,7 +172,9 @@ def datetime_filter(t):
 
 async def init(loop):
     await orm.create_pool(loop=loop, **configs.db)
-    app = web.Application(loop=loop, middlewares=[logger_factory, response_factory])
+    app = web.Application(loop=loop, middlewares=[
+        logger_factory, auth_factory, response_factory
+    ])
     init_jinja2(app,filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
     add_static(app)
